a8d7f3e3fb1e27e883d5e5a7afce6cc5ddfd2e5c
thiago65535 <138196440+thiago65535@users.noreply.github.com>
2023-06-30 16:46:24 +0800

    HomeBanner 添加展示图片
    HomeBanner 添加展示图片

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -1,31 +1,14 @@
 import React, { memo, useEffect } from 'react'
 import hyRequest from '@/services'
 import { useState } from 'react'
-
+import {HomeWrapper} from './style'
+import HomeBanner from './c-cpns/home-banner'
 const Home = memo(() => {
-  // 定义状态
-  const [ highScore, setHighScore ] = useState({})
-
-  // 网络请求的代码
-  useEffect(() => {
-    hyRequest.get({ url: "/home/highscore" }).then(res => {
-      console.log(res)
-      setHighScore(res)
-    })
-  }, [])
-
+  
   return (
-    <div>
-      <h2>{highScore.title}</h2>
-      <h4>{highScore.subtitle}</h4>
-      <ul>
-        {
-          highScore.list?.map((item) => {
-            return <li key={item.id}>{item.name}</li>
-          })
-        }
-      </ul>
-    </div>
+    <HomeWrapper>
+        <HomeBanner/>
+    </HomeWrapper>
   )
 })
 
